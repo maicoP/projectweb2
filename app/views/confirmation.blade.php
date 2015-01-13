5513d649ee83d0e8968cf9ca94ede69cdf403924
Matthias Verhoeven <matthiasverhoeven@gmail.com>
2015-01-13 10:45:09 +0100

    Confirmation

--- app/views/confirmation.blade.php
+++ app/views/confirmation.blade.php
@@ -8,8 +8,8 @@
 	</div>
 	{{ HTML::image('css/img/thatsit.png') }}
 	<h1>Your ball of friendship will soon be sent to {{$reciever}}!</h1>	
-	<p>
-		Do you want to save your ball of friendship and send them to other people
+	<h1>
+		Want to save your ball or send it to more people? Please
 		@if(Auth::check())
 			{{Form::open(['url' => '/card/save'])}}
 			{{Form::hidden('image',$image)}}
@@ -18,8 +18,8 @@
 		@else
 			{{Link_to('/users/login','login')}}
 		@endif
-		
-	</p>
+		 to do so.
+	</h1>
 
 	 <a href="/" class="btn">Create another one?</a>
 @stop
